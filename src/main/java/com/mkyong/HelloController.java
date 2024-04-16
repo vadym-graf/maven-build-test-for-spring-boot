e269dc4419d059aa01bd7f3637411da069d03be6
paula.alemand@saveincloud.com <paulaisdev@gmail.com>
2024-04-16 11:27:28 -0300

    added comment line test 1

--- src/main/java/com/mkyong/HelloController.java
+++ src/main/java/com/mkyong/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+// teste
     @RequestMapping("/")
     String hello() {
         return "Hello World, Spring Boot!";
